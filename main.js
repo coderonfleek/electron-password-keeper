82d125ec8ad0bf2d691ab5af0b919e10056e5361
Fikayo <fik4christ@yahoo.com>
2018-07-08 01:22:15 -0700

    Login Working, Tokens Saved

--- main.js
+++ main.js
@@ -1,17 +1,95 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, protocol } = require("electron");
+const httpRequest = require("request");
+const { URL, URLSearchParams } = require("url");
+
+const Store = require("electron-store");
+const store = new Store();
+
+/* require("es6-promise").polyfill();
+require("isomorphic-fetch"); */
 
 let win;
+
+const customScheme = "custom-scheme";
+const customDomain = "custom-domain";
+const auth0Domain = "fikitout.auth0.com";
+const client_id = "Ml52S0dL3GHJUuL4Q8Vl7aeL540pELiM";
+const client_secret =
+  "F3DqOf0xQOponkxe_yZ8oNrnqPYCWbcuzlrhW52Nr1-YSqDGQaoFwdZyUv2IoBL7";
+
+// needed, otherwise localstorage, sessionstorage, cookies, etc, become unavailable
+// https://electronjs.org/docs/api/protocol#methods
+protocol.registerStandardSchemes([customScheme]);
 
 function showWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 1000,
     height: 600,
-    webPreferences: { webSecurity: false }
+    webPreferences: {
+      nodeIntegration: false
+    }
   });
 
+  protocol.registerFileProtocol(
+    customScheme,
+    (request, callback) => {
+      const url = request.url
+        .replace(`${customScheme}://${customDomain}/`, "")
+        .substring(0, request.url.length - 1);
+
+      if (url.indexOf("home.html") === 0) {
+        // needed, otherwise it will try to load a non-existing file ending with '#access_token=eyJ0...'
+
+        const myURL = new URL(request.url);
+        const code = myURL.searchParams.get("code");
+        console.log(code);
+
+        // fetch(`https://${auth0Domain}/oauth/token`, {
+        //   method: "POST",
+        //   headers: new Headers({
+        //     "Content-Type": "text/plain"
+        //   })
+        // }).then(function(data) {
+        //   /* handle response */
+        //   console.log(data);
+        // });
+
+        var options = {
+          method: "POST",
+          url: `https://${auth0Domain}/oauth/token`,
+          headers: { "content-type": "application/json" },
+          body: {
+            grant_type: "authorization_code",
+            client_id: client_id,
+            client_secret: client_secret,
+            code: code,
+            redirect_uri: `${customScheme}://${customDomain}/home.html`
+          },
+          json: true
+        };
+
+        httpRequest(options, function(error, response, body) {
+          if (error) throw new Error(error);
+
+          console.log(body);
+          //win.localStorage.setItem(body.id_token);
+          store.set("access_token", body.access_token);
+          console.log(store.get("access_token"));
+        });
+
+        return callback(`${__dirname}/home.html`);
+      }
+
+      callback(`${__dirname}/${url}`);
+    },
+    error => {
+      if (error) console.error("Failed to register protocol");
+    }
+  );
+
   // and load the index.html of the app.
-  win.loadFile("index.html");
+  win.loadURL(`${customScheme}://${customDomain}/index.html`);
 
   // Open the DevTools.
   win.webContents.openDevTools();
@@ -46,6 +124,3 @@ app.on("activate", () => {
     showWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
